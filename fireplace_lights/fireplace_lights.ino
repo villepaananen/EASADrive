a92522633298900f6c250dcf6811f5c77c077b22
Ville Paananen <villepaananen@outlook.com>
2020-08-07 12:56:47 +0300

    fireplace

---
+++ fireplace_lights/fireplace_lights.ino
@@ -0,0 +1,84 @@
+#include <multi_channel_relay.h>
+  
+Multi_Channel_Relay relay;
+const int trigPin = 2;
+const int echoPin = 3;
+
+int delayAmount = 100;
+long duration;
+int distance;
+int distance2; 
+
+int counter = 0;
+
+int activationDistance = 30;
+
+void setup() {
+  Serial.begin(9600);
+  relay.begin(0x11);
+  randomSeed(analogRead(0));
+  pinMode(trigPin, OUTPUT);
+}
+
+void loop() {
+  distance = readDistance();
+  
+  // Prints the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.println(distance);
+
+  if (distance < activationDistance) {
+    for (int i=1; i<=4; i++) {
+      delayAmount = 500 - i * 50;
+      relay.turn_on_channel(i);
+      delay(delayAmount);
+    }
+
+    distance2 = readDistance();
+    Serial.print("Distance2: ");
+    Serial.println(distance2);
+
+    if (distance2 < activationDistance) {
+      fadeUp();
+    } else {
+      for (int j=4; j>=1; j--) {
+        relay.turn_off_channel(j);
+        delay(100);
+      }
+    } 
+  }
+}
+
+void fadeUp() {
+  for (int i=1; i<=4; i++) {
+    delayAmount = 300 - i * 50;
+    relay.turn_off_channel(i);
+    delay(delayAmount);
+  }
+
+  for (int i=1; i<=4; i++) {
+    delayAmount = 300 - i * 50;
+    relay.turn_on_channel(i);
+    delay(delayAmount);
+  }
+
+}
+
+int readDistance() {
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  
+  // Calculating the distance
+  distance= duration*0.034/2;
+
+  return distance;
+}
--- middle_lights/middle_lights.ino
+++ middle_lights/middle_lights.ino
@@ -4,14 +4,18 @@ Multi_Channel_Relay relay;
 const int trigPin = 2;
 const int echoPin = 3;
 
-int delayAmount = 500;
+int delayAmount = 100;
 long duration;
 int distance;
+
+int counter = 0;
+
+int activationDistance = 30;
 
 void setup() {
   Serial.begin(9600);
   relay.begin(0x11);
-
+  randomSeed(analogRead(0));
   pinMode(trigPin, OUTPUT);
 }
 
@@ -35,43 +39,53 @@ void loop() {
   Serial.print("Distance: ");
   Serial.println(distance);
 
-  if (distance < 5) {
+  if (distance < activationDistance) {
+    // do stuff
     flash();
-  }
-
-  for (int counter = 0; counter <=7; counter++) {
-    if (counter == 0) {
+  } else {
+  
+    delayAmount = 1000 + random(-30, 30);
+  
+    switch (counter) {
+    case 1:
       relay.turn_on_channel(1);
       delay(delayAmount);
-    }
-    if (counter == 1) {
+      break;
+    case 2:
       relay.turn_off_channel(4);
       delay(delayAmount);
-    }
-    if (counter == 2) {
+      break;
+    case 3:
       relay.turn_on_channel(2);
       delay(delayAmount);
-    }
-    if (counter == 3) {
+      break;
+    case 4:
       relay.turn_off_channel(1);
       delay(delayAmount);
-    }
-    if (counter == 4) {
+       break;
+    case 5:
       relay.turn_on_channel(3);
       delay(delayAmount);
-    }
-    if (counter == 5) {
+      break;
+    case 6:
       relay.turn_off_channel(2);
       delay(delayAmount);
-    }
-    if (counter == 6) {
+      break;
+    case 7:
       relay.turn_on_channel(4);
       delay(delayAmount);
-    }
-    if (counter == 7) {
+      break;
+    case 8:
       relay.turn_off_channel(3);
       delay(delayAmount);
-    }    
+      break;
+    }
+    
+    if (counter == 8) {
+      counter = 1;
+    } else {
+      counter++;
+    }
   }
 }
 
